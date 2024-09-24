26bdc733c8495a9f5612b4e9db7f48c1ad8ec0ba
Anderson Acuña <andersonyhavs@outlook.com>
2024-09-23 19:29:30 -0500

    Agrego función para mostrar tablero.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+struct Board([i8; 5], [i8; 5], [i8; 5], [i8; 5], [i8; 5]);
+
+impl Board {
+    fn show(&self) {
+        for column in self.0.iter() {
+            print!("{column} ");
+        }
+        println!();
+        for column in self.1.iter() {
+            print!("{column} ");
+        }
+        println!();
+        for column in self.2.iter() {
+            print!("{column} ");
+        }
+        println!();
+        for column in self.3.iter() {
+            print!("{column} ");
+        }
+        println!();
+        for column in self.4.iter() {
+            print!("{column} ");
+        }
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board([1, 1, 1, 1, 1], 
+                             [2, 2, 2, 2, 2], 
+                             [3, 3, 3, 3, 3], 
+                             [4, 4, 4, 4, 4], 
+                             [5, 5, 5, 5, 5]);
+
+    board.show();
+    println!();
+    board.show();
 }
